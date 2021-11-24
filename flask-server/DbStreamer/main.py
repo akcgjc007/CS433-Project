498b845ea18315af37627fdc06e0a370d8948f98
akcgjc007 <akcgjc007@gmail.com>
2021-11-24 12:11:10 +0530

    Restructured and reformatted code.

---
+++ flask-server/DbStreamer/main.py
@@ -0,0 +1,217 @@
+import mysql.connector as mysql
+
+
+class DbStreamer:
+    def __init__(self):
+        self.conn = mysql.connect(
+            host="localhost", user="root", passwd="0000", db="mydb")
+        self.run_my_query("use mydb;")
+        return
+
+    def __del__(self):
+        self.conn.commit()
+        self.conn.close()
+        return
+
+    def run_my_query(self, command):
+        _cursor = self.conn.cursor()
+        _cursor.execute(command)
+        results = _cursor.fetchall()
+        return results
+
+    def initialize_database(self):
+        self.run_my_query("drop database if exists mydb;")
+        self.run_my_query("create database mydb;")
+        self.run_my_query("use mydb;")
+
+    def insert_into_users(self, id, name, email, password):
+        sql = "insert into Users (id, name, email, password) values (%s, %s, %s, %s);"
+        val = (id, name, email, password)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def insert_into_questions(self, title, description, user_id):
+        sql = "insert into Questions (title, description, user_id) values ( %s, %s, %s);"
+        val = (title, description, user_id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def find_qid(self, title):
+        sql = "select id from Questions where title = %s;"
+        val = (title)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val,))
+        results = _cursor.fetchall()
+        return results
+
+    def insert_into_answers(self, description, question_id, user_id):
+        sql = "insert into Answers (description, question_id, user_id) values ( %s, %s, %s);"
+        val = (description, question_id, user_id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def insert_into_tags(self, name, question_id):
+        sql = "insert into Tags (name, question_id) values (%s, %s);"
+        val = (name, question_id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def update_question(self, id, description):
+        sql = "update Questions set description = %s where id = %s;"
+        val = (description, id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def delete_question(self, id):
+        res = self.run_my_query(
+            "select id from Answers where question_id = "+str(id)+";")
+        for i in res:
+            self.delete_answer(i[0])
+
+        sql1 = "delete from QuestionVotes where question_id = %s;"
+        sql2 = "delete from Tags where question_id = %s;"
+        sql3 = "delete from Questions where id = %s;"
+        val = (id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql1, (val, ))
+        _cursor.execute(sql2, (val, ))
+        _cursor.execute(sql3, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def update_answer(self, id, description):
+        sql = "update Answers set description = %s where id = %s;"
+        val = (description, id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def delete_answer(self, id):
+        sql1 = "delete from AnswerVotes where answer_id = %s;"
+        sql2 = "delete from Answers where id = %s;"
+        val = (id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql1, (val, ))
+        _cursor.execute(sql2, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def upvote_question(self, user_id, question_id):
+        sql1 = "insert into QuestionVotes (user_id, question_id) values (%s, %s);"
+        sql2 = "update Questions set upvotes = upvotes+1 where id = %s;"
+        val1 = (user_id, question_id)
+        val2 = (question_id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql1, (val1))
+        _cursor.execute(sql2, (val2, ))
+        results = _cursor.fetchall()
+        return results
+
+    def upvote_answer(self, user_id, answer_id):
+        sql1 = "insert into AnswerVotes (user_id, answer_id) values (%s, %s);"
+        sql2 = "update Answers set upvotes = upvotes+1 where id = %s;"
+        val1 = (user_id, answer_id)
+        val2 = (answer_id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql1, (val1))
+        _cursor.execute(sql2, (val2, ))
+        results = _cursor.fetchall()
+        return results
+
+    def search_question(self, title):
+        sql = """
+        select * from Questions
+        where title like concat("%", %s, "%");
+        """
+        val = (title)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def search_question_by_id(self, id):
+        sql = "select * from Questions where id = %s;"
+        val = (id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def search_answers(self, id):
+        sql = "select * from Answers where question_id = %s order by upvotes DESC;"
+        val = (id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def search_by_tag(self, tag):
+        sql = "select * from Tags where name = %s;"
+        val = (tag)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def get_top_questions(self):
+        sql = "select * from Questions limit 10;"
+        val = ()
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql)
+        results = _cursor.fetchall()
+        return results
+
+    def check_user(self, email, password):
+        sql = "select * from Users where email = %s and password = %s;"
+        val = (email, password)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def insert_into_tokens(self, user_id, token):
+        sql = "insert into Tokens (user_id, token) values (%s, %s);"
+        val = (user_id, token)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val))
+        results = _cursor.fetchall()
+        return results
+
+    def check_token(self, token):
+        sql = "select user_id from Tokens where token = %s;"
+        val = (token)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val,))
+        results = _cursor.fetchall()
+        return results
+
+    def search_answer_by_id(self, id):
+        sql = "select * from Answers where id = %s;"
+        val = (id)
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql, (val, ))
+        results = _cursor.fetchall()
+        return results
+
+    def get_tables(self):
+        sql = "SHOW TABLES;"
+        _cursor = self.conn.cursor()
+        _cursor.execute(sql)
+        data = _cursor.fetchall()
+        for i in data:
+            print(i[0])
+            res = self.run_my_query("select * from "+i[0])
+            for j in res:
+                print(j)
+            print()
---
+++ flask-server/api/ApiHandlers.py
@@ -0,0 +1,212 @@
+from flask_restful import Api, Resource, reqparse
+from flask import request
+from DbStreamer.main import DbStreamer
+from uuid import uuid4
+
+
+class AllQuestionsHandler(Resource, DbStreamer):
+    def get(self):
+        data = self.get_top_questions()
+        return {
+            'resultStatus': 'SUCCESS',
+            'message': "Hello Api Handler",
+            'data': data
+        }
+
+
+class QuestionHandler(Resource, DbStreamer):
+    def get(self, id):
+        question_data = self.search_question_by_id(id)
+        answers_data = self.search_answers(id)
+
+        return {
+            'resultStatus': 'SUCCESS',
+            'question_data': question_data,
+            'answer_data': answers_data
+        }
+
+
+class Signup(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        userid = data.get('userid', '')
+        name = data.get('name', '')
+        email = data.get('email', '')
+        password = data.get('password', '')
+
+        self.insert_into_users(userid, name, email, password)
+
+        res = {"status": "Success",
+               "message": "Account created successfully."}
+
+        return res
+
+
+class Login(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        email = data.get('email', '')
+        password = data.get('password', '')
+
+        # check if this username and password are good or not
+        query_res = self.check_user(email, password)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Login unsuccessful."}
+
+            return res
+        else:
+            rand_token = uuid4()
+            self.insert_into_tokens(query_res[0][0], str(rand_token))
+
+            res = {"status": "Success",
+                   "message": "Login successfully.",
+                   "userid": query_res[0][0],
+                   "name": query_res[0][1],
+                   "token": str(rand_token)}
+            return res
+
+
+class AddQuestion(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        token = data.get('token', '')
+        title = data.get('title', '')
+        desc = data.get('desc', '')
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Adding Question unsuccessful."}
+
+            return res
+        else:
+            self.insert_into_questions(title, desc, query_res[0][0])
+            r = self.find_qid(title)
+
+            res = {"status": "Success",
+                   "message": "Question added successfully.",
+                   "qid": r[0][0]
+                   }
+            return res
+
+
+class AddAnswer(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        token = data.get('token', '')
+        desc = data.get('desc', '')
+        qid = data.get('qid', '')
+        print(token, desc, qid)
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Adding Question unsuccessful."}
+
+            return res
+        else:
+            self.insert_into_answers(desc, qid, query_res[0][0])
+
+            res = {"status": "Success",
+                   "message": "Answer added successfully."
+                   }
+            return res
+
+
+class DeleteQuestion(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        token = data.get('token', '')
+        qid = data.get('qid', '')
+        print(token, qid)
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Adding Question unsuccessful."}
+
+            return res
+        else:
+            qInfo = self.search_question_by_id(qid)
+            assert(qInfo[0][3] == query_res[0][0])
+
+            self.delete_question(qid)
+            res = {"status": "Success",
+                   "message": "Question deleted added successfully."
+                   }
+            return res
+
+
+class DeleteAnswer(Resource, DbStreamer):
+    def post(self):
+        print("Coming here.")
+        data = request.get_json()
+        token = data.get('token', '')
+        aid = data.get('aid', '')
+        print(token, aid)
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Deleting answer unsuccessful."}
+
+            return res
+        else:
+            aInfo = self.search_answer_by_id(aid)
+            assert(aInfo[0][3] == query_res[0][0])
+
+            self.delete_answer(aid)
+            res = {"status": "Success",
+                   "message": "Answer deleted successfully."
+                   }
+            return res
+
+
+class UpvoteQuestion(Resource, DbStreamer):
+    def post(self):
+        data = request.get_json()
+        token = data.get('token', '')
+        qid = data.get('qid', '')
+        print(token, qid)
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Adding Question unsuccessful."}
+
+            return res
+        else:
+            # qInfo = self.search_question_by_id(qid)
+            # assert(qInfo[0][1] == query_res[0][0])
+
+            self.upvote_question(query_res[0][0], qid)
+            res = {"status": "Success",
+                   "message": "Question upvoted added successfully."
+                   }
+            return res
+
+
+class UpvoteAnswer(Resource, DbStreamer):
+    def post(self):
+        print("Coming here.")
+        data = request.get_json()
+        token = data.get('token', '')
+        aid = data.get('aid', '')
+        print(token, aid)
+        query_res = self.check_token(token)
+
+        if len(query_res) == 0:
+            res = {"status": "Fail",
+                   "message": "Deleting answer unsuccessful."}
+
+            return res
+        else:
+            # aInfo = self.search_answer_by_id(aid)
+            # assert(aInfo[0][3] == query_res[0][0])
+
+            self.upvote_answer(query_res[0][0], aid)
+            res = {"status": "Success",
+                   "message": "Answer upvoted successfully."}
+            return res
